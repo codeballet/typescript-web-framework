9e9cc6fd547e162651a1f80b83d0247f55b5fd1f
johan <stjernholm@msn.com>
2020-09-14 12:29:21 +0800

    Feature: add view CollectionView

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-## Requirements
+# The Web Framework
 
-### Global dependencies to install
+This simple web framework is a study in how to put together an object-oriented TypeScript application.
 
-`npm install -g parcel-bundler`
+The application uses a mixture of composition and inheritance in the relations between the objects, depending on what is more appropriate for each individual case.
+
+The framework is built along the lines of logic that guided early web frameworks that used to be popular before the rise of more recent libraries / frameworks such as React. As a reference, the 'View' section of the framework has similarities to [Marionette](https://marionettejs.com/), and the 'Model' section has similarities to [Backbone.js](https://backbonejs.org/). Marionette and Backbone are commonly used together.
+
+## Usage
+
+I do not recommend to use this web framework in actual web app building practice, as it is very rudimentary. Instead, I recommend to use the more fully featured, already existing frameworks referred to above: Marionette and Backbone.js.
+
+## License
+
+MIT
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const user = User.buildUser({ name: 'Thor', age: 99 });
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.getElementById('root');
+users.fetch();
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
+users.on('change', () => {
+  const root = document.getElementById('root');
 
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error('Root element not found');
-}
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+// import { UserEdit } from './views/UserEdit';
+// import { User } from './models/User';
+
+// const user = User.buildUser({ name: 'Thor', age: 99 });
+
+// const root = document.getElementById('root');
+
+// if (root) {
+//   const userEdit = new UserEdit(root, user);
+
+//   userEdit.render();
+//   console.log(userEdit);
+// } else {
+//   throw new Error('Root element not found');
+// }
